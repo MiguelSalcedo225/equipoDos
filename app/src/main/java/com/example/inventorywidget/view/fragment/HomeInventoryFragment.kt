d805a9614bbbc25a8dcf3261c12ba94d27c4bbe8
MiguelSalcedo225 <miguelangelsalcedo2020@gmail.com>
2025-11-08 18:14:43 -0500

    Ícono flotante de agregar producto

--- app/src/main/java/com/example/inventorywidget/view/fragment/HomeInventoryFragment.kt
+++ app/src/main/java/com/example/inventorywidget/view/fragment/HomeInventoryFragment.kt
@@ -1,28 +1,33 @@
 package com.example.inventorywidget.view.fragment
 
 import android.os.Bundle
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
-import androidx.core.view.isVisible
+import android.view.*
 import androidx.fragment.app.Fragment
-import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
-import androidx.recyclerview.widget.LinearLayoutManager
 import com.example.inventorywidget.R
 import com.example.inventorywidget.databinding.FragmentHomeBinding
-import com.example.inventorywidget.view.adapter.InventoryAdapter
-import com.example.inventorywidget.viewmodel.InventoryViewModel
-class HomeInventoryFragment : Fragment() {
-    private lateinit var binding: FragmentHomeBinding
-    private val inventoryViewModel: InventoryViewModel by viewModels()
+
+class HomeFragment : Fragment() {
+    private var _binding: FragmentHomeBinding? = null
+    private val binding get() = _binding!!
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
-        binding = FragmentHomeBinding.inflate(inflater)
-        binding.lifecycleOwner = this
+    ): View {
+        _binding = FragmentHomeBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        setupClickListeners()
+    }
+
+    private fun setupClickListeners() {
+        binding.fabAdd.setOnClickListener {
+            findNavController().navigate(R.id.action_homeFragment_to_addProductFragment)
+        }
     }
 }
